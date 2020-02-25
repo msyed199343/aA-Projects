f444c000ae122f22014cd05f1cac31292cbc658e
Syed Mohiuddin <msyed199343@gmail.com>
2020-02-25 01:16:17 -0600

    adding js files from hw

--- JavaScript/practice_problems.js
+++ JavaScript/practice_problems.js
@@ -51,3 +51,74 @@ function mysteryScoping5() {
 // mysteryScoping3();
 // mysteryScoping4();
 mysteryScoping5();
+
+
+function madLib(verb, adj, noun) {
+   console.log( `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`);
+
+}
+
+madLib('make', 'best', 'guac');
+
+function isSubstring(searchString, subString){
+    if (searchString.includes(subString)){
+        console.log(true)
+    }
+    else {
+        console.log(false)
+    };
+
+}
+
+isSubstring("time to program", "time")
+isSubstring("Jump for joy", "joys")
+
+function fizzBuzz(array) {
+    const arr = []
+    array.forEach(num => {
+        if ((num % 3 === 0 || num % 5 === 0) && !(num % 3 === 0 && num % 5 === 0)){
+            arr.push(num)
+        }
+
+    });
+    console.log(arr)
+}
+
+let arr_1 = [1, 2, 5, 10] //[5, 10]
+fizzBuzz(arr_1)
+
+function isPrime(number) {
+    
+  for (let num = 2; num < number; num++) {
+  if (number % num === 0 ){
+      return false;
+  }
+ 
+  
+  };
+  
+  return true;
+}
+
+console.log(isPrime(2))
+console.log(isPrime(10))
+console.log(isPrime(15485863))
+console.log(isPrime(3548563))
+console.log(isPrime(1))
+
+function sumOfNPrimes(n) {
+    let sum = 0
+    let count = 0;
+    let i = 2
+    while (count < n){
+        if (isPrime(i)){sum += i
+            count++ 
+        };
+        i++;
+    };
+    return sum
+}
+
+console.log(sumOfNPrimes(0))
+console.log(sumOfNPrimes(1))
+console.log(sumOfNPrimes(4))
