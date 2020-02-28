7c79e9fe3d127cf4ce6990e6e99cf5d90f1ea76b
Syed Mohiuddin <msyed199343@gmail.com>
2020-02-28 04:51:03 -0600

    merge sort in js

--- JavaScript/Intro_to_JS_exercises/phase_4_recursion.js
+++ JavaScript/Intro_to_JS_exercises/phase_4_recursion.js
@@ -67,3 +67,81 @@ function fibonacci(n) {
 
 // [0] + 
 console.log(fibonacci(11)); //[0, 1, 1]
+
+function deepDup(arr){
+    if(!(arr instanceof Array)){
+        return arr;
+    }
+
+    return arr.map((el) => {
+        return deepDup(el);
+    });
+};
+
+console.log(deepDup([1, 1, 1, 2, 3, 5]));
+
+//without recursion solve 
+function bsearch(arr, target){
+    if (!arr.includes(target)) {return -1};
+    if (arr.includes(target)) {return arr.indexOf(target)};
+};
+
+//with recursion
+
+function bsearch_2(arr, target){
+    if (arr.length === 0 ){
+        return -1
+    };
+
+    const mid_idx = Math.floor(arr.length/2)
+    const mid = arr[mid_idx]
+
+    if (target === mid){
+        return mid_idx
+    }
+    else if (target < mid) {
+        const left = arr.slice(0, mid_idx)
+        return bsearch_2(left, target)
+    }
+    else {
+        const right = arr.slice(mid_idx+1)
+        const func = bsearch_2(right, target)
+        return func === -1 ? -1 : func + (mid_idx +1)
+    };
+
+};
+
+console.log(bsearch_2([1,2,3,4,5], 5)) // 4
+console.log(bsearch([1, 2, 3, 4, 5], 5)) // 4
+
+function combine (left, right){
+   const arr = [];
+    
+    while (left.length > 0 && right.length > 0 ) {
+        if (left[0] < right[0]){
+          arr.push(left.shift()) }  
+        else {
+            
+            arr.push(right.shift());
+        };
+    };
+
+    return arr
+
+};
+
+function mergeSort(arr){
+    if (arr.length < 2  ){return arr}
+    else {
+    const middle = Math.floor(arr.length/2);
+
+    const left_side = mergeSort(arr.slice(0, middle)); //b/a[3] // [2]
+    const right_side = mergeSort(arr.slice(middle)); // b [2, 1] a  // [1]
+
+    return combine(left_side, right_side)};
+};
+//[3, 2, 1]
+                    // [9, 1, 5]  [6, 4, 3, 2]
+console.log(mergeSort([9, 1, 5, 6, 4, 3, 2])) //[1, 2, 3, 4. 5. 6. 9]
+console.log(mergeSort([3, 2, 1]))
+console.log(combine([5, 1, 6], [3, 5, 7]))
