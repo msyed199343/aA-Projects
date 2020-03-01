4e67500a6ae985fcc0e06bf151adfc54d518e8f8
Syed Mohiuddin <msyed199343@gmail.com>
2020-03-01 04:34:16 -0600

    Reversi game

---
+++ JavaScript/Reversi_Part_1.js
@@ -0,0 +1,30 @@
+//2 players each representing color on baord 
+
+function Board(){
+
+};
+
+//1. place 4 discs in middle 2 black 2 white with corresponding colors
+//diagnol to each other so... 
+  //  [B, W],
+  //  [W, B]}
+  //2. choose color for each player (black first)
+  //3. black puts a piece so if a black pice is on both sides
+  // of white picer it flips to black
+  //next white same thing 
+  //play until no more moves can be made by other player 
+  //lose turn if you cant place disk to flip opsoing players disk 
+  ///player with most disks on the board wins!
+
+//64 discs reversible one side black other side white 
+function Piece(){
+
+};
+
+function Game(){
+
+};
+
+Game.prototype.placePiece = function(position, color){
+    
+};
