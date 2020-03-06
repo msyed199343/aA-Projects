30578778770def8f019ec411a4ed8c34f43040b7
Syed Mohiuddin <msyed199343@gmail.com>
2020-03-06 01:05:43 -0600

    practice problems

--- JavaScript/simple_functions.js
+++ JavaScript/simple_functions.js
@@ -1,15 +1,57 @@
-const readline = require('readline');
+// setTimeout(function () {
+//     alert('HAMMER TIME!');
+// }, 5000);
+
+// // // Timeout with a Closure
+
+// function hammerTime(time) {
+//     window.setTimeout(function () {
+//         alert(`${time} is HAMMER TIME!`);
+//     });
+// }
 
 
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
-reader.question('Would you like some tea?', (res) =>{
-    console.log(`You replied "${res}."`);
-});
 
-reader.question('Would you like biscuits?', (res)=>{
-    console.log(`You replied "${res}".`)
-});
+// const readline = require('readline');
+// let first, second;
+
+// const reader = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+// function teaAndBiscuits() {
+
+//     reader.question('Would you like some tea?', function(res) {
+//         console.log(`You replied "${res}."`);
+//     });
+
+//         reader.question('Would you like biscuits?', function (res){
+//             console.log(`You replied "${res}".`)
+//         });
+
+//     const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+//     const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+//     console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+//     reader.close();
+// };
+
+function Cat() {
+    this.name = 'Funky';
+    this.age = 3;
+}
+
+function Dog() {
+    this.name = 'Spunky';
+    this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+    console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Spunky = new Dog();
+const Funky = new Cat();
+
+Spunky.chase.call(Spunky, Funky);
+Spunky.chase.apply(Spunky, [Funky])
